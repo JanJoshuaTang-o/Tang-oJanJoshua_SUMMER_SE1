d21aec9570d0afc2769fb209ec01b1601624f78c
Jan Joshua Tang-o <70315490+JanJoshuaTang-o@users.noreply.github.com>
2021-06-28 14:22:03 +0800

    Create script5.js
    added script5.js

---
+++ js/script5.js
@@ -0,0 +1,12 @@
+middle = (array) =>{
+asa = array.length;
+sa = 0;
+for(i=0; i<array.length; i++){
+sa = array/2;
+}
+if(sa == 0)
+return array[(asa/2)], array[(asa/2)+1]
+else if(sa == 1){
+return array[asa/2]
+}
+}
