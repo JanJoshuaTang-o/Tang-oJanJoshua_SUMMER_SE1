cabf41f96904fb15ee56561b802961726deaf1fa
Jan Joshua Tang-o <70315490+JanJoshuaTang-o@users.noreply.github.com>
2021-06-28 14:59:14 +0800

    Update script9.js

--- js/script9.js
+++ js/script9.js
@@ -3,9 +3,12 @@ firstName ="Jan Joshua";
 lastName = "Tang-o";
 age = 21;
 walk(distance){
-return "I can walk " + distance + "kilometers";
+return "I can walk " + this.distance + "kilometers";
 }
 }
 class Student extends Person{
 studID = 2018111575;
+ enroll(subject){
+ return "I enrolled in the " + this.subject + "subject";
+ }
 }
