43de5068b2fbf384a62b1e2e5223c7abc7034bb1
Jan Joshua Tang-o <70315490+JanJoshuaTang-o@users.noreply.github.com>
2021-06-28 14:54:27 +0800

    Create script9.js
    added script9.js

---
+++ js/script9.js
@@ -0,0 +1,11 @@
+class Person{
+firstName ="Jan Joshua";
+lastName = "Tang-o";
+age = 21;
+walk(distance){
+return "I can walk " + distance + "kilometers";
+}
+}
+class Student extends Person{
+studID = 2018111575;
+}
