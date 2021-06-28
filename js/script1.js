98aa5dad2b1207802ab4420a82d1ca517b13f716
Jan Joshua Tang-o <70315490+JanJoshuaTang-o@users.noreply.github.com>
2021-06-28 13:55:34 +0800

    Create script1.js
    added script1.js

---
+++ js/script1.js
@@ -0,0 +1,10 @@
+function myself(){
+{firstname: "Jan Joshua"  
+lastname: "Tang-o" 
+age: 20 
+subject: "ICS 2609" 
+}}
+function introduce() {
+    return "Hello, I am Jan Joshua  Tang-o."} 
+function enrolled() { 
+    return "I am enrolled in ICS 2609."}
