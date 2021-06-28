617e64406fc5fb9f68eb68e9bcec7761a6835100
Jan Joshua Tang-o <70315490+JanJoshuaTang-o@users.noreply.github.com>
2021-06-28 14:42:52 +0800

    Create script7.js
    added script7.js

---
+++ js/script7.js
@@ -0,0 +1,9 @@
+squares = [4,6,2,9,5]
+function sideSquare(squa){
+for(i=0;i<squa.length; i++){
+art = squa[i];
+return area[i] = art/4;
+}
+return area;
+}
+Xarea = squares.map(sideSquare(squares));
